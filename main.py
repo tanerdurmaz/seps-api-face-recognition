38e8348cfeb22057a2dc78c6691712348df17793
hexap <47399791+closedAi@users.noreply.github.com>
2021-04-25 20:05:13 +0300

    critical

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from flask import request, jsonify
 from PIL import Image, ImageDraw
 import requests
 import numpy as np
+import face_recognition
+
+
+# This is an example of running face recognition on a single image
+# and drawing a box around each person that was identified.
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file("biden.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Joe Biden"
+]
+print('Learned encoding for', len(known_face_encodings), 'images.')
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
