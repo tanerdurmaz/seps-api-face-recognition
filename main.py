5f5d9d8efb21eaa19c50c88a0e16660ed6c1f967
hexap <47399791+closedAi@users.noreply.github.com>
2021-04-25 19:10:32 +0300

    mistake

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>seps xd.</p>"
 
 @app.route('/api', methods=['GET'])
 def api():
