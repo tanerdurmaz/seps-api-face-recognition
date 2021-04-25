78e4ed02da6ea3eab84a5316a4391be4c0b4e957
hexap <47399791+closedAi@users.noreply.github.com>
2021-04-25 20:11:57 +0300

    so close

--- main.py
+++ main.py
@@ -42,3 +42,26 @@ def api():
 	password= request.args.get('password')
 	print(password)
 	return "<h1>Distant  Archive</h1>" + username + " " + password
+
+@app.route('/req', methods=['GET'])
+def req():
+	# Load an image with an unknown face
+	unknown_image = face_recognition.load_image_file("obama2.jpg")
+
+	# Find all the faces and face encodings in the unknown image
+	face_locations = face_recognition.face_locations(unknown_image)
+	face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+	
+	name = "Unknown"
+	for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+	    # See if the face is a match for the known face(s)
+	    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+	    name = "Unknown"
+
+	    # Or instead, use the known face with the smallest distance to the new face
+	    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+	    best_match_index = np.argmin(face_distances)
+	    if matches[best_match_index]:
+	        name = known_face_names[best_match_index]
+	return name
