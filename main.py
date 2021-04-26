af4b8fc53b2cadd1c3874246cb18117f5dab24cb
hexap <47399791+closedAi@users.noreply.github.com>
2021-04-26 18:18:06 +0300

    decode bug

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def api():
 @app.route('/find/<url>', methods=['GET'])
 def find(url):
     #url = urllib.parse.unquote_plus(url)
-    #if url:
-    #	return url
+    if url:
+    	return url
     #url = url.replace("*", "/")
     im = Image.open(requests.get(url, stream=True).raw)
     im = im.convert('RGB')
